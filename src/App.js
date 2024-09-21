7145194175f392f96b603f85564c0c0229065a04
Selimo Shano <mokherhi@gmail.com>
2024-09-21 18:39:33 +0200

    iniyui

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Footer/FOOTER.JSX';
+import Navbar from './NAVBAR/NAVBAR.JSX';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer /> 
+    </div>
+  );
+}
+
+export default App;
